6f2c8e90695dc718e40e39b5780e1e8675eb983e
Kairat619 <wkz777@gmail.com>
2024-04-28 12:55:00 +0600

    profile added

--- content/_data/siteconfig.js
+++ content/_data/siteconfig.js
@@ -6,7 +6,7 @@ module.exports = {
     // Site URL to generate absolute URLs. Used across the board.
     url: process.env.URL || "http://localhost:8080",
     // Profile image for left sidebar
-    image: "/assets/images/sidebar_profile.jpg",
+    image: "/assets/images/sidebar_profile_dev.png",
     // Image alt text for left sidebar
     imageAlt: "René and Einstein",
     // Author name, shown in left sidebar, and used in JSON-LD
@@ -16,7 +16,7 @@ module.exports = {
     // OpenGraph default image, in case you don't have an `image`
     // set in your Markdown frontmatter; relevant for social
     // sharing.
-    openGraphDefaultImage: "/assets/images/opengraph.jpg",
+    openGraphDefaultImage: "/assets/images/opengrap-dev.png",
     // GitHub ID (optional, remove it not needed), used for link in the left sidebar
     socialGitHub: "muenzpraeger",
     // LinkedIn ID  (optional, remove it not needed), used for link in the left sidebar
